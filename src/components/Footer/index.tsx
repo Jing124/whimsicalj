d5c2a69624ca48dd8c532b6846c54aa16e1acb03
Guanghui Li <liguanghui.cs@gmail.com>
2022-07-14 21:24:52 -0400

    footer mobile

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import './index.scss';
 
 import Container from '../widgets/Container';
+import ExternalLink from '../widgets/Link/ExternalLink/ExternalLink';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
@@ -8,24 +9,30 @@ const Footer = () => {
 	return (
 		<footer className='pt-10 pb-20'>
 			<Container>
-				<div className='grid grid-cols-2'>
+				<div className='grid sm:grid-cols-2 gap-5'>
 					<div className='flex justify-center'>
-						<div>
+						<div className='text-center sm:text-start'>
 							<div className='font-bold'>Email</div>
 							<div className='mt-2'>wjing.design97@gmail.com</div>
 						</div>
 					</div>
 					<div className='flex justify-center'>
-						<div>
-							<div>Follow Me</div>
+						<div className='text-center sm:text-start'>
+							<div className='font-bold'>Follow Me</div>
 							<div className='mt-2'>
 								<div className='flex flex-wrap items-center gap-1'>
-									<LinkedInIcon />
-									<InstagramIcon />
-									<img
-										src={`${process.env.PUBLIC_URL}/icons/flickr.webp`}
-										alt='flickr'
-									/>
+									<ExternalLink href='https://www.linkedin.com/in/jing-wang-80101920a/'>
+										<LinkedInIcon />
+									</ExternalLink>
+									<ExternalLink href='https://www.instagram.com/no_idddddea/'>
+										<InstagramIcon />
+									</ExternalLink>
+									<ExternalLink href='https://www.flickr.com/photos/189946022@N05/albums'>
+										<img
+											src={`${process.env.PUBLIC_URL}/icons/flickr.webp`}
+											alt='flickr'
+										/>
+									</ExternalLink>
 								</div>
 							</div>
 						</div>
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,17 @@
 import Footer from '../Footer';
 import Navbar from '../Navbar';
 import { ReactNode } from 'react';
+import useMinHeight from '../../models/util_models/ScreenUtilModel/hooks/useMinHeight';
+import useNavbarHeight from '../../models/util_models/ScreenUtilModel/hooks/useNavbarHeight';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+	const navbarHeight = useNavbarHeight();
+	const minHeight = useMinHeight(navbarHeight);
+
 	return (
 		<>
 			<Navbar />
-			{children}
+			<div style={{ minHeight: `${minHeight}px` }}>{children}</div>
 			<Footer />
 		</>
 	);
