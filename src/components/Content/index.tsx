bc12a4485035e7afd1adbb823f6c005d6a3cb488
Guanghui Li <liguanghui.cs@gmail.com>
2022-07-14 20:30:00 -0400

    content desktop

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,33 @@
+import './index.scss';
+
+import Container from '../widgets/Container';
+import PropTypes from 'prop-types';
+import { ReactNode } from 'react';
+
+const Content = ({
+	className = '',
+	title,
+	children,
+	...attributes
+}: {
+	className?: string;
+	title?: string;
+	children: ReactNode;
+}) => {
+	return (
+		<Container className={`Content ${className}`} {...attributes}>
+			{title && (
+				<div className={`title py-24 text-5xl font-extrabold`}>
+					{title ?? ''}
+				</div>
+			)}
+			<div className={`${title ? 'pt-10' : 'pt-40'}`}>{children}</div>
+		</Container>
+	);
+};
+
+Content.propTypes = {
+	id: PropTypes.string,
+};
+
+export default Content;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,41 @@
 import './index.scss';
 
+import Content from '../Content';
+
 const Home = () => {
-	return <div id='Home'>Home</div>;
+	return (
+		<Content id='Home'>
+			<div className='grid grid-cols-2 gap-10'>
+				<div className='flex justify-center'>
+					<video
+						controls
+						poster={`${process.env.PUBLIC_URL}/videos/intro/poster.png`}
+					>
+						<source
+							src={`${process.env.PUBLIC_URL}/videos/intro/intro.mp4`}
+							type='video/mp4'
+						/>
+						Your browser does not support the video tag.
+					</video>
+				</div>
+				<div className='flex justify-center'>
+					<div>
+						<div className='text-8xl font-extrabold hello'>
+							Hello
+						</div>
+						<div className='mt-5 text-4xl font-bold'>
+							A Bit About Me
+						</div>
+						<div className='mt-10 w-80'>
+							I'm a motion & 3D modeling designer. As a graduate,
+							I am new to the field and hope to learn more
+							knowledge through job opportunities.
+						</div>
+					</div>
+				</div>
+			</div>
+		</Content>
+	);
 };
 
 export default Home;
